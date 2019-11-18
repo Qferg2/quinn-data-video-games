3b06d0e303e670cdf4406f0c33cc51fdfc7d053c
Qferg2 <54326424+Qferg2@users.noreply.github.com>
2019-11-18 09:06:09 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -3,7 +3,10 @@ import os
 import json
 app = Flask(__name__)
 
-
+@app.route("/")
+def render_main():
+    with open('video_games.json') as videogame_data:
+        counties = json.load(videogame_data)
 
 if __name__=="__main__":
     app.run(debug=False, port=54321)
