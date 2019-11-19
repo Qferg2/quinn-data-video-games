3aead92d5fc19889dc8dab09b7796272240f074e
Qferg2 <54326424+Qferg2@users.noreply.github.com>
2019-11-19 10:01:43 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -6,8 +6,21 @@ app = Flask(__name__)
 @app.route("/")
 def render_main():
     with open('video_games.json') as videogame_data:
-        counties = json.load(videogame_data)
+        games = json.load(videogame_data)
         return render_template('home.html')
-
+        
+        
+def get_game_options(games):
+    options = ""
+    listOfTitles = []
+    for game in games:
+        if game['Title'] not in listOfTitles:
+            listOfTitles.append(game['Title'])
+            
+    for game in listOfTitles:
+        options = options + Markup("<option value=\"" + state + "\">" + state + "</option>")
+    return options
+        
+        
 if __name__=="__main__":
     app.run(debug=False, port=54321)
