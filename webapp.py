6c64826a8da39ca6cbc25aa520beec08d564bbac
Qferg2 <54326424+Qferg2@users.noreply.github.com>
2019-11-21 08:48:20 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -8,6 +8,18 @@ def render_main():
     with open('video_games.json') as videogame_data:
         games = json.load(videogame_data)
         return render_template('home.html')
+    
+@app.route("/p1")
+def render_page1():
+    return render_template('page1.html')
+
+@app.route("/p2")
+def render_page2():
+    return render_template('page2.html')
+
+@app.route("/p3")
+def render_page3():
+    return render_template('page3.html')
         
         
 def get_game_options(games):
