e652bc890511bf9ec2cd543501c436990bd7244c
Qferg2 <54326424+Qferg2@users.noreply.github.com>
2019-12-03 09:57:23 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -19,7 +19,7 @@ def interesting_info():
         games = json.load(videogame_data)
     game= request.args["game"]
     print(game)
-    return render_template('page1.html', options = get_game_options(games), name = game, info = get_interesting_info(games, game), info1 = get_interesting_info1(games, game))
+    return render_template('page1.html', options = get_game_options(games), name = game, info = get_interesting_info(games, game), info1 = get_interesting_info1(games, game), info2 = get_interesting_info2(games, game))
 
 @app.route("/p2")
 def render_page2():
@@ -38,6 +38,16 @@ def get_interesting_info1(games, game):
     for data in games:
         if data['Title'] == game:
             return data['Release']['Year']
+            
+def get_interesting_info2(games, game):
+    for data in games:
+        if data['Title'] == game:
+            return data['Metrics']['Sales']
+            
+def get_interesting_info3(games, game):
+    for data in games:
+        if data['Title'] == game:
+            return data['Metrics']['Review Score']
         
 def get_game_options(games):
     options = ""
