1e6f59a85f54afbf43077d062dca08efc58ed6ae
Qferg2 <54326424+Qferg2@users.noreply.github.com>
2019-12-05 09:09:36 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -17,13 +17,14 @@ def render_page1():
 def interesting_info():
     with open('video_games.json') as videogame_data:
         games = json.load(videogame_data)
-    game= request.args["game"]
-    print(game)
+    game = request.args["game"]
     return render_template('page1.html', options = get_game_options(games), name = game, info = get_interesting_info(games, game), info1 = get_interesting_info1(games, game), info2 = get_interesting_info2(games, game), info3 = get_interesting_info3(games, game))
 
 @app.route("/p2")
 def render_page2():
-    return render_template('page2.html')
+    with open('video_games.json') as videogame_data:
+        games = json.load(videogame_data)
+    return render_template('page2.html', yearOptions = get_year_options(games))
 
 @app.route("/p3")
 def render_page3():
@@ -59,6 +60,17 @@ def get_game_options(games):
     for game in listOfTitles:
         options = options + Markup("<option value=\"" + game + "\">" + game + "</option>")
     return options
+    
+def get_year_options(games):
+    yearOptions = ""
+    listOfYears = []
+    for game in games:
+        if game['Release']['Year'] not in listOfYears:
+            listOfYears.append(game['Release']['Year'])
+            
+    for game in listOfYears:
+        options = options + Markup("<option value=\"" + game + "\">" + game + "</option>")
+    return yearOptions
         
         
 if __name__=="__main__":
